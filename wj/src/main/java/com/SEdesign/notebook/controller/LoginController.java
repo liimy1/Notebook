6198dd898177c664b37d8891a8f82d54784a705d
limy <1511268879@qq.com>
2021-01-05 18:27:58 +0800

    连接数据库

--- wj/src/main/java/com/SEdesign/notebook/controller/LoginController.java
+++ wj/src/main/java/com/SEdesign/notebook/controller/LoginController.java
@@ -5,21 +5,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.HtmlUtils;
 import com.SEdesign.notebook.pojo.User;
-import java.util.Objects;
+import com.SEdesign.notebook.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+
 @Controller
 public class LoginController {
 
+    @Autowired
+    UserService userService;
+
     @CrossOrigin
-    @PostMapping(value = "api/login")
+    @PostMapping(value = "/api/login")
     @ResponseBody
     public Result login(@RequestBody User requestUser) {
-        // 对 html 标签进行转义，防止 XSS 攻击
         String username = requestUser.getUsername();
         username = HtmlUtils.htmlEscape(username);
 
-        if (!Objects.equals("admin", username) || !Objects.equals("123456", requestUser.getPassword())) {
-            String message = "账号密码错误";
-            System.out.println("test");
+        User user = userService.get(username, requestUser.getPassword());
+        if (null == user) {
             return new Result(400);
         } else {
             return new Result(200);
