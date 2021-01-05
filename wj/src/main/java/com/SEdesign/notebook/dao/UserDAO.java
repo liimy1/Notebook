e4b08eb24adbf2ae085ec1bff4d9476b01e2f4f8
limy <1511268879@qq.com>
2021-01-05 15:14:32 +0800

    后端开发登录页面

---
+++ wj/src/main/java/com/SEdesign/notebook/dao/UserDAO.java
@@ -0,0 +1,9 @@
+package com.SEdesign.notebook.dao;
+
+import com.SEdesign.notebook.pojo.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserDAO extends JpaRepository<User,Integer> {
+    User findByUsername(String username);
+    User getByUsernameAndPassword(String username,String password);
+}
