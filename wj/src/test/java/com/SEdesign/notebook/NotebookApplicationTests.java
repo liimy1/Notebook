ce5e25a4034b72819fc6fce69321866052e4523d
limy <1511268879@qq.com>
2021-01-05 14:37:23 +0800

    Initial commit

---
+++ wj/src/test/java/com/SEdesign/notebook/NotebookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SEdesign.notebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotebookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
